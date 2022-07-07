4199aea2bff147eb3f97739d2edb07e65911a671
Tigran <gvideo2007@gmail.com>
2022-07-07 21:56:02 +0400

    React.FunctionComponent.

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, { FC, useState } from 'react';
+
+export enum CardVariant {
+  outlined = 'outlined',
+  primary = 'primary'
+}
 
 interface CardProps {
   width?: string;
   height?: string;
   children?: React.ReactNode;
+  onClick: (num: number) => void;
+  variant: CardVariant;
 }
 
-const Card = ({ width, height, children }: CardProps) => {
+debugger;
 
+const Card: FC<CardProps> = ({ width, height, variant, onClick, children }) => {
+  const [state, setState] = useState(0);
   return (
-    <div style={{ width, height, border: '1px solid gray' }}>
+    <div style={{ width, height, border: variant === CardVariant.outlined ? '1px solid gray' : 'none', background: variant === CardVariant.primary ? 'lightgray' : '' }}
+      onClick={() => onClick(state)}
+    >
       {children}
     </div>
   );
